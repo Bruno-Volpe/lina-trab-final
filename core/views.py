ad372860627c564d5f84df731081d66addd35f76
Bruno-Volpe <bruno.gvolpe@icloud.com>
2024-12-14 17:40:57 -0300

    feat: add lisdt groups

--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def home_view(request):
+    # if not logged in, redirect to login page
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html')
     return render(request, 'core/home.html')
